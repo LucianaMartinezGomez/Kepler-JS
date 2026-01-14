0bd816b8e540d829ca6237cffd91db8c8e3e947c
salva608 <totoalahielmejor@gmail.com>
2026-01-14 11:44:01 -0500

    guardar spa

--- spa-ejercicios/app.js
+++ spa-ejercicios/app.js
@@ -2,19 +2,19 @@ const app = document.getElementById('app')
 
 
 function renderHome(){
-    app.innertHTML = '<h1> Home </h1><p>Bienvenido a nuestro SPA </p>'
+    app.innerHTML = '<h1> Home </h1><p>Bienvenido a nuestro SPA </p>';
 }
 
 function renderServices(){
-    app.innertHTML = '<h1> Servicios </h1> <p>Fronted con JS </p>'
+    app.innerHTML = '<h1> Servicios </h1><p>Fronted con JS </p>';
 }
 
 function renderContact(){
-    app.innertHTML = '<h1> Contacto </h1> <p>clan@hamilton </p>'
+    app.innerHTML = '<h1> Contacto </h1><p>clan@hamilton </p>';
 }
 
-document.getElementById('home').addEventListener('click', renderHome)
-document.getElementById('services').addEventListener('click', renderServices)
-document.getElementById('contact').addEventListener('click', renderContact)
+document.getElementById('home').addEventListener('click', renderHome);
+document.getElementById('services').addEventListener('click', renderServices);
+document.getElementById('contact').addEventListener('click', renderContact);
 
 renderHome()
